be30beb3322097c8158931e0c19d5d0e61d90654
luccahuguet <lucca.huguet2@gmail.com>
2024-04-22 17:53:32 -0300

    loads individual meals

---
+++ components/MealItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealItem = ({ title }) => {
+  return (
+    <View style={styles.container}>
+      <Text>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default MealItem;
